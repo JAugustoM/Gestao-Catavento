2e7c2ff91879c943baa0f8838b3c29c2f2540a8f
JAugustoM <joseaugustomoraes@outlook.com>
2024-11-25 22:06:01 -0300

    [FEAT] Inicialização do Supabase funcionando

--- catavento/lib/main.dart
+++ catavento/lib/main.dart
@@ -1,21 +1,45 @@
 import 'package:catavento/constants.dart';
 import 'package:catavento/views/login_view.dart';
 import 'package:flutter/material.dart';
-import 'screens/dashboard_admin.dart'; // Importando a tela DashboardAdmin
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: "Gestão Catavento",
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      useMaterial3: true,
+    ),
+    home: const LoadView(),
+    routes: {
+      loginRoute: (context) => const LoginView(),
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class LoadView extends StatelessWidget {
+  const LoadView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter DashBoard',
-      // A tela inicial será a DashBoardAdmin
-      home: DashBoardAdmin(),
+    return FutureBuilder(
+      future: Supabase.initialize(
+          url: "https://gfxmrqhwjqekfzrlurqr.supabase.com",
+          anonKey: String.fromEnvironment("ANON_KEY")),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final supabase = Supabase.instance.client;
+            final user = supabase.auth.currentUser;
+            if (user != null) {
+              return const Text("Hey");
+            } else {
+              return const LoginView();
+            }
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
     );
   }
 }
