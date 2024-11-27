f85a98cafb387765998143ac9fb25045c39a337f
Prg-maker <df3296709@gmail.com>
2024-11-27 19:50:55 -0300

    feat: Adicionei o Dialog estilizado, Também coloquei um butão para fechar o dialog Co-authored-by: Samara Alves <samaramaria0220@gmail.com> Co-authored-by: Leticia Arisa <higaleticia11@gmail.com>

--- catavento/lib/screens/dashboard_admin.dart
+++ catavento/lib/screens/dashboard_admin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class DashBoardAdmin extends StatelessWidget {
   const DashBoardAdmin({super.key});
@@ -6,23 +7,154 @@ class DashBoardAdmin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
-      body: Center(
-        child: ElevatedButton(onPressed: (){
-          
-        },
-          style: ElevatedButton.styleFrom(
-            backgroundColor:  Color(0xFF1A73E8),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(22)
-            )
-          ),
-          child: Text("Adicionar demanda" , style: TextStyle(
-            color: Colors.white,
 
-          ),),
-        ),
+      //Esse Center pode ser alterado, só coloquei de exemplo.
+      body: Center(
+
+        // Esse ElevatedButton é o botão que abre o dialog.
+        child: ElevatedButton(
+            onPressed: () {
+              _showCustomDialog(context);
+            },
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF1A73E8),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(22))),
+            child: Text(
+              "Adicionar demanda",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            )),
       ),
     );
   }
+}
+// Dialog
+void _showCustomDialog(BuildContext context){
+  showGeneralDialog(
+    context: context,
+    pageBuilder: (context, animation1, animation2) {
+      return Container();
+    },
+    transitionBuilder: (context, a1, a2, widget) {
+      return ScaleTransition(
+        scale: Tween<double>(begin: 0.5, end: 1.0).animate(a1),
+
+        child: AlertDialog(
+          backgroundColor: Color(0xFFD1EEFF),
+          content: SizedBox(
+              height: 639,
+              width: 560,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 41),
+                child: Container(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Expanded(
+
+                              child: Align(
+                                //Aqui ficará o nome da demanda ou qual demanda será
+                                alignment: Alignment.center,
+                                child: Text("Nome da demanda",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              )),
+                          IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: Icon(
+                                Icons.close,
+                                size: 25,
+                              ))
+                        ],
+                      ),
+                      SizedBox(height: 31),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 186,
+                            height: 202,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                              BorderRadius.circular(20),
+                            ),
+                          ),
+                          SizedBox(width: 30),
+                          Column(
+                            children: [
+                              RichText(
+                                  text: TextSpan(
+                                      text: "Código: ",
+                                      style: TextStyle(
+                                          fontWeight:
+                                          FontWeight.bold,
+                                          fontSize:15),
+
+                                      children: [
+                                        TextSpan(
+                                            text: "{Codigo do bolos}")
+                                      ])),
+                              SizedBox(height: 10),
+                              RichText(
+                                  text: TextSpan(
+                                      text: "Código: ",
+                                      style: TextStyle(
+                                          fontWeight:
+                                          FontWeight.bold,
+                                          fontSize:15),
+
+                                      children: [
+                                        TextSpan(
+                                            text: "{Codigo do bolos}")
+                                      ])),
+                              SizedBox(height: 10),
+
+                              RichText(
+                                  text: TextSpan(
+                                      text: "Código: ",
+                                      style: TextStyle(
+                                          fontWeight:
+                                          FontWeight.bold,
+                                          fontSize:15),
+
+                                      children: [
+                                        TextSpan(
+                                            text: "{Codigo do bolos}")
+                                      ])),
+                              SizedBox(height: 10),
+
+                              RichText(
+                                  text: TextSpan(
+                                      text: "Código: ",
+                                      style: TextStyle(
+                                          fontWeight:
+                                          FontWeight.bold,
+                                          fontSize:15),
+
+                                      children: [
+                                        TextSpan(
+                                            text: "{Codigo do bolos}")
+                                      ])),
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )),
+          shape: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16.0),
+              borderSide: BorderSide.none),
+        ),
+      );
+    },
+  );
 }
