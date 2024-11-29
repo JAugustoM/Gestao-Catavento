60d6f22ab3d6056b6507558b118af87bc69d90e6
JAugustoM <joseaugustomoraes@outlook.com>
2024-11-29 12:04:27 -0300

    [FEAT] Alteração da planilha para prevenir duplicação de demandas

--- catavento/lib/services/table_import/table_import.dart
+++ catavento/lib/services/table_import/table_import.dart
@@ -1,18 +1,19 @@
 import 'dart:io';
 // import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:excel/excel.dart';
-import 'package:path_provider/path_provider.dart';
 
 Future<void> importExcelToSupabase(String filePath) async {
   // final supabase = Supabase.instance.client;
   try {
-    final fileBytes = File(filePath).readAsBytesSync();
-    final excel = Excel.decodeBytes(fileBytes);
+    final file = File(filePath);
+    final excel = Excel.decodeBytes(file.readAsBytesSync());
 
     for (var sheetName in excel.tables.keys) {
       final sheet = excel.tables[sheetName];
 
       if (sheet != null) {
+        excel.link(sheetName, sheet);
+        var rowId = 1;
         print('processando planilha: $sheetName');
         for (var row in sheet.rows) {
           // mapear os valores das celular para uma lista de dados puros
@@ -21,23 +22,28 @@ Future<void> importExcelToSupabase(String filePath) async {
 
           // print('dados da linha: $rowData');
 
-          var rowId = 1;
-
           // verifica a validade da coluna (se é nula ou vazia)
           if (rowData.isNotEmpty && rowData.first.isNotEmpty) {
             try {
-              if (rowData[0] != "" && rowData[2] != 'X') {
+              if (rowData[0] != "" && rowData[2] != "X") {
                 final Map<String, dynamic> parsedData = {
                   'nomeDemanda': rowData[0],
                   'funcionario': rowData[1].isNotEmpty == true ? rowData[1] : 0,
+                  'status': rowData[2].isNotEmpty == true ? rowData[2] : 'R',
                 };
 
-                print(
-                    'dados lidos: ${parsedData['nomeDemanda']} - ${parsedData['funcionario']}');
+                var demanda = parsedData['nomeDemanda'];
+                var funcionario = parsedData['funcionario'];
+                var status = parsedData['status'];
+
+                print('Linha $rowId: $demanda - $funcionario - $status');
                 // await supabase.from('demandas').insert(parsedData);
 
-                var cell = sheet.cell(CellIndex.indexByString('C$rowId'));
-                cell.value = TextCellValue('X$rowId');
+                var cell = sheet.cell(CellIndex.indexByColumnRow(
+                  columnIndex: 2,
+                  rowIndex: rowId,
+                ));
+                cell.value = TextCellValue('X');
                 rowId++;
               }
             } catch (e) {
@@ -45,14 +51,10 @@ Future<void> importExcelToSupabase(String filePath) async {
             }
           }
         }
+        print(rowId);
       }
     }
 
-    final splitPath = filePath.split('/');
-    final fileName = splitPath.last;
-
-    final directory = await getApplicationDocumentsDirectory();
-    final file = File('${directory.path}/$fileName');
     await file.writeAsBytes(excel.encode()!);
   } catch (e) {
     print('erro ao importar o arquivo: $e');
--- catavento/lib/services/table_import/table_picker.dart
+++ catavento/lib/services/table_import/table_picker.dart
@@ -1,18 +1,30 @@
+import 'dart:io';
+
 import 'package:file_picker/file_picker.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<String?> tablePicker() async {
   //retorna o caminho do arquivo
   try {
-    final dir = await getApplicationDocumentsDirectory();
+    final dir = await getDownloadsDirectory();
     final result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
       allowedExtensions: ['xlsx'],
-      initialDirectory: dir.path,
+      initialDirectory: dir!.path,
     );
 
     if (result != null && result.files.isNotEmpty) {
-      return result.files.single.path; //retornar caminho do arquivo
+      final path = result.files.single.path;
+      final file = File(path!);
+      final newFileName = path.split('/').last;
+      final newFilePath = '${dir.path}/$newFileName';
+      final newFile = File('${dir.path}/$newFileName');
+
+      if (!await newFile.exists()) {
+        await file.copy(newFilePath);
+      }
+
+      return newFilePath;
     }
   } catch (e) {
     print("Erro escolhendo o arquivo: $e");
