f025d5796a646acb9f7783e802deb76bb22d99dd
Danielle Soares <silvasoaresdanielle2@gmail.com>
2024-12-09 18:45:29 -0300

    feature: menu finalizado, janela navbar funcionando

--- catavento/lib/screens/components/menu.dart
+++ catavento/lib/screens/components/menu.dart
@@ -1,101 +1,46 @@
 import 'package:flutter/material.dart';
 
-class IconMenu extends StatefulWidget {
-  const IconMenu({super.key});
-
-  @override
-  State<IconMenu> createState() {
-    return IconMenuState();
-  }
-}
-
-class IconMenuState extends State<IconMenu> {
-  void openMenu(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (context) {
-        return Container(
-          height: 300,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(16),
-              topRight: Radius.circular(16),
-            ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                color: Colors.pink[100],
-                padding: EdgeInsets.all(16),
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Colors.grey,
-                      radius: 25,
-                    ),
-                    SizedBox(width: 16),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'João das Couves',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          'Administrador',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey[700],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              ListTile(
-                title: Text('Demandas'),
-                onTap: () {
-                  // Ação para Demandas
-                },
-              ),
-              ListTile(
-                title: Text('Funcionários'),
-                onTap: () {
-                  // Ação para Funcionários
-                },
-              ),
-              ListTile(
-                title: Text('Produtos'),
-                onTap: () {
-                  // Ação para Produtos
-                },
-              ),
-              ListTile(
-                title: Text('Sair da Conta'),
-                onTap: () {
-                  // Ação para Sair da Conta
-                },
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
+class Navbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      iconSize: 35,
-      onPressed: () => openMenu(context),
-      icon: Icon(Icons.menu),
-      color: Color(0xFF015C98),
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: const Text(
+              'João das Couves', // depois alterar aqui pra pegar o nome no banco de dados
+              style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+            ),
+            accountEmail: const Text(
+              'Administrador', // depois alterar aqui pra pegar o setor no banco de dados
+              style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+            ),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.grey.shade200,
+              child: const Icon(Icons.person),
+            ),
+            decoration: BoxDecoration(
+              color: Color(0xFFF4D5D7),
+            ),
+          ),
+          ListTile(
+            title: const Text('Demandas'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Funcionários'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Produtos'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Sair da Conta'),
+            onTap: () {},
+          ),
+        ],
+      ),
     );
   }
 }
