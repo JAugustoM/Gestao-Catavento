93aae069d48efc1356eb9481c22d4e5a3ed37f42
Prg-maker <df3296709@gmail.com>
2024-12-10 18:05:12 -0300

    feat: refatorando o demandCart

--- catavento/lib/screens/components/demandCard.dart
+++ catavento/lib/screens/components/demandCard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:catavento/bloc/demanda_bloc.dart';
 
 import 'package:catavento/screens/components/confirmDialog.dart';
+
 class DemandCard extends StatelessWidget {
   final String nomeDemanda;
   final String status;
@@ -27,11 +28,11 @@ class DemandCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final TextEditingController _nomeController =
-    TextEditingController(text: nomeDemanda);
+        TextEditingController(text: nomeDemanda);
     final TextEditingController _codigoController =
-    TextEditingController(text: codigo);
+        TextEditingController(text: codigo);
     final TextEditingController _descricaoController =
-    TextEditingController(text: descricao);
+        TextEditingController(text: descricao);
 
     return Card(
       margin: EdgeInsets.symmetric(vertical: 8.0),
@@ -75,7 +76,7 @@ class DemandCard extends StatelessWidget {
                       return ConfirmDialog(
                         title: 'Confirmar Exclusão',
                         contente:
-                        'Tem certeza de que deseja apagar esta demanda?',
+                            'Tem certeza de que deseja apagar esta demanda?',
                         onConfirm: () {
                           Navigator.of(context).pop(); // Fecha o diálogo
                           bloc.add(DemandaDelete(id, order)); // BACKEND
@@ -89,3 +90,105 @@ class DemandCard extends StatelessWidget {
       ),
     );
   }
+
+  // Função para mostrar as informações da demanda em um diálogo
+  void _showInfoDialog(BuildContext context, String nome, String codigo,
+      String descricao, String status) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Informações da Demanda"),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Nome: $nome",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                Text("Código: $codigo",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                Text("Status: $status",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                Text("Descrição: $descricao",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            ),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context); // Fechar o diálogo
+              },
+              child: Text("Fechar"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Função que exibe o diálogo de edição
+  void _showEditDialog(
+      BuildContext context,
+      TextEditingController nomeController,
+      TextEditingController codigoController,
+      TextEditingController descricaoController,
+      DemandaBloc bloc) // BACKEND
+  {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Editar Demanda"),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(
+                  controller: nomeController,
+                  decoration: InputDecoration(labelText: "Nome da Demanda"),
+                ),
+                TextField(
+                  controller: codigoController,
+                  decoration: InputDecoration(labelText: "Código da Demanda"),
+                ),
+                TextField(
+                  controller: descricaoController,
+                  decoration:
+                      InputDecoration(labelText: "Descrição da Demanda"),
+                  maxLines: null, // Permite múltiplas linhas
+                  minLines: 4,
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                // Atualizar a demanda no Supabase
+                bloc.add(DemandaUpdate(
+                  id,
+                  order,
+                  nomeController.text,
+                  codigoController.text,
+                  descricaoController.text,
+                )); // BACKEND
+                //_updateDemanda(nomeController.text, codigoController.text,descricaoController.text);
+                Navigator.pop(context); // Fechar o diálogo
+              },
+              child: Text("Salvar"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context); // Fechar o diálogo sem salvar
+              },
+              child: Text("Cancelar"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
