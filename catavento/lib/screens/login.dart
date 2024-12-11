a7baf5c670366538c575b85f1ced7f2905874fd8
Prg-maker <df3296709@gmail.com>
2024-12-11 20:25:59 -0300

    feat: Button SingIn criado e estilizado

--- catavento/lib/screens/login.dart
+++ catavento/lib/screens/login.dart
@@ -1,13 +1,14 @@
+import 'dart:js_interop';
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'components/input_purple.dart';
-
-
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
+    bool isLoading = false;
     double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Stack(
@@ -32,7 +33,6 @@ class Login extends StatelessWidget {
             child: Container(
               width: 400.0,
               height: 400.0,
-
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10),
                 color: Colors.white,
@@ -42,32 +42,58 @@ class Login extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   // Outros widgets aqui
-                  SizedBox(height: 100,),
-                  PurpleTextField(label: "Digite o nome do seu usuário", icon: Icon( Icons.person_outline , color: Color(0xCCACACAC),),),
-                  PurpleTextField(label: "Digite a sua senha", icon: Icon( Icons.person_outline , color: Color(0xCCACACAC),),),
+                  SizedBox(
+                    height: 100,
+                  ),
+                  PurpleTextField(
+                    label: "Digite o nome do seu usuário",
+                    icon: Icon(
+                      Icons.person_outline,
+                      color: Color(0xCCACACAC),
+                    ),
+                  ),
+                  PurpleTextField(
+                    label: "Digite a sua senha",
+                    icon: Icon(
+                      Icons.lock_outline,
+                      color: Color(0xCCACACAC),
+                    ),
+                  ),
+                  ButtonSingIn(
+                    title: Text("Sing in"),
+                    isLoading: isLoading,
+                    icon: Icon(
+                      Icons.keyboard_arrow_right_rounded,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      isLoading= !isLoading;
+                      log("IsLoading  $isLoading");
+                    },
+                  ),
 
                   //SizedBox(height: 35),
-
-                  ElevatedButton( // colocar o botão para a direita 
-                    onPressed: () {}, 
+                  ElevatedButton(
+                    // colocar o botão para a direita
+                    onPressed: () {},
                     style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 40, vertical: 15),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30),
                       ),
                       backgroundColor: Color.fromARGB(255, 28, 126, 238),
                     ),
 
-                      child: Text(
-                        "Entrar", 
-                        style: TextStyle ( 
-                          fontSize: 18,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-          
                 ],
               ),
             ),
@@ -75,7 +101,8 @@ class Login extends StatelessWidget {
 
           Center(
             child: Transform.translate(
-              offset: Offset(0, -230), // Mover a imagem 50 pixels para cima (ajuste conforme necessário)
+              offset: Offset(0, -230),
+              // Mover a imagem 50 pixels para cima (ajuste conforme necessário)
               child: Image.asset(
                 "assets/images/cake.png",
                 width: 128,
@@ -83,9 +110,57 @@ class Login extends StatelessWidget {
               ),
             ),
           ),
-         
         ],
       ),
+    );
+  }
+}
+
+class ButtonSingIn extends StatefulWidget {
+  final Widget title;
+  final Icon icon;
+  final bool isLoading;
+
+  final Function() onPressed;
+
+  const ButtonSingIn(
+      {super.key,
+      required this.title,
+      required this.icon,
+      required this.onPressed,
+      required this.isLoading});
+
+  @override
+  State<ButtonSingIn> createState() => _ButtonSingInState();
+}
+
+class _ButtonSingInState extends State<ButtonSingIn> {
+  @override
+
+
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap:(){
+        widget.onPressed();
+        setState(() {
+        });
+      },
+
+
+      child: Container(
+          height: 45,
+          width: 114,
+          decoration: BoxDecoration(
+              color: Color(0xFF75CDF3),
+              borderRadius: BorderRadius.circular(8.0)),
+          child: Center(
+              child: widget.isLoading
+                  ? Transform.scale( scale: 0.5,child: CircularProgressIndicator(color:Color(0xFFED5EA3 ) , backgroundColor: Colors.white,))
+                  : Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [widget.title, widget.icon,
+                      ],
+                    ))),
     );
   }
 }
