a64bbf248b6790e6afbe95ec19c293aa57ad67d7
Danielle Soares <silvasoaresdanielle2@gmail.com>
2024-12-14 17:19:47 -0300

    fix: bug do menu desapareceu

--- catavento/lib/screens/dashboardAdmin/dashboard_admin.dart
+++ catavento/lib/screens/dashboardAdmin/dashboard_admin.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:catavento/bloc/demanda_bloc.dart';
 import 'package:catavento/bloc/demanda_controller.dart';
-import 'package:catavento/screens/employee-management.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
@@ -200,7 +199,6 @@ class IconMenuState extends State<IconMenu> {
         iconSize: 50,
         onPressed: () {
           //Logica do menu
-    
         },
         icon: Icon(Icons.menu),
         color: Color(0xFF015C98),
