98408c57c89c5dffb8cf767a342039fb27b05ce8
Danielle Soares <silvasoaresdanielle2@gmail.com>
2024-12-14 17:40:29 -0300

    fix: rota para página de demandas

--- catavento/lib/screens/employee-management.dart
+++ catavento/lib/screens/employee-management.dart
@@ -3,12 +3,57 @@ import 'components/background.dart';
 import 'components/header.dart';
 import 'components/blocks.dart';
 import 'components/showDialog.dart';
+import 'package:catavento/screens/components/menu.dart';
 
 class EmployeeManagement extends StatelessWidget {
   const EmployeeManagement({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Navbar(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: const IconThemeData(
+          color: Color(0xFF015C98),
+        ),
+        leading: Builder(
+          builder: (BuildContext context) {
+            return Padding(
+              padding: const EdgeInsets.only(left: 20), // Padding aqui
+              child: Material(
+                color: Colors.transparent, // Mantém o fundo transparente
+                child: InkWell(
+                  onTap: () {
+                    Scaffold.of(context).openDrawer(); // Abre o Drawer
+                  },
+                ),
+              ),
+            );
+          },
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(
+                right: 20), // Ajuste o valor conforme necessário
+            child: TextButton.icon(
+              icon: const Icon(Icons.history, size: 18.0),
+              label: const Text("Ver Histórico"),
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: const Color(0xFF015C98),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      extendBodyBehindAppBar: true,
       body: BackgroundPage(
         gradientColors: [Color(0xFF75CDF3), Color(0xFFB2E8FF)],
         children: [
