fbd95281175edbeab4244cd8d18cc64398da4d90
prg-maker <df3296709@gmail.com>
2025-01-04 15:36:10 -0300

    feat:Terminando de estilizar o menu e colocando um novo package de icon

--- catavento/lib/screens/Funcionario/dashboardfuncionario.dart
+++ catavento/lib/screens/Funcionario/dashboardfuncionario.dart
@@ -1,5 +1,8 @@
+import 'package:catavento/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import './components/menuBar.dart';
 
 class DashBoardFuncionario extends StatelessWidget {
   const DashBoardFuncionario({super.key});
@@ -10,13 +13,18 @@ class DashBoardFuncionario extends StatelessWidget {
     return Scaffold(
       drawer: Navbar(),
       appBar: AppBar(
-        title: Text("Side Bar"),
+
         leading: Builder(
           builder: (context) => IconButton(
               onPressed: () => {
                     Scaffold.of(context).openDrawer(),
                   },
-              icon: Icon(Icons.tune, color: Colors.black)),
+              icon: Icon(Icons.tune, color: Colors.black),
+              hoverColor: Colors.transparent,
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+            ),
+
         ),
       ),
       body: Container(
@@ -25,68 +33,5 @@ class DashBoardFuncionario extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class Navbar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Drawer(
-        child: Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              stops: [0.99, 1.0],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFF3598D4), // Cor #3598D4
-                Color(0xFF015C98), //
-              ])),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-
-            child: Center(
-
-              child: Column(
-
-                children: [
-                  SizedBox(height: 20,),
-                    Container(
-                      height: 60,
-                      width: 60,
-                      decoration:BoxDecoration(
-                        color: Color(0xFFE1DCE6),
-                        borderRadius: BorderRadius.circular(100)
-                      ) ,
-                      child: Center(
-                        child:Icon(Icons.person_outline , size: 30, color: Color(00000000).withOpacity(0.45),),
-                      ),
-                    ),
-                  SizedBox(height: 16,),
-                  Column(
-                    children: [
-                      Text("Lucas Almeida" , style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold
-                      )),
-                      Text("Produção de Bolos Artificiais" , style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12
-                      ))
-                    ],
-                  )
-
-
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    ));
   }
 }
