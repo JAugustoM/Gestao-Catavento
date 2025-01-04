7195108f0cad070357a237fc90ae81532cca0045
prg-maker <df3296709@gmail.com>
2025-01-04 20:39:44 -0300

    feat:adicionando o card demanda

--- catavento/lib/screens/Funcionario/dashboardfuncionario.dart
+++ catavento/lib/screens/Funcionario/dashboardfuncionario.dart
@@ -25,22 +25,64 @@ class DashBoardFuncionario extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(
-       padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-        ),
+      body: Row(
+        children: [
+          CardDemanda()
+        ],
+      ),
+    );
+  }
+}
+
+class CardDemanda extends StatelessWidget {
+  const CardDemanda({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height / 1.2;
+
+    return Container(
+      width: 300,
+      height: screenHeight,
+      decoration: BoxDecoration(
+          color: Color(0xFFEBEBEB), borderRadius: BorderRadius.circular(12)),
+      child: Padding(
+        padding: EdgeInsets.only(top: 20, left: 7.5, right: 7.5, bottom: 0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Dadosfuncionarios(
-              nome: 'fulano',
-              nomeusuario: 'nomeusuario',
-              email: 'email',
-              setor: 'setor',
+            Text(
+              "nome da demanda",
+              style: TextStyle(
+                  color: Color(0xFF015C98),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
             ),
+            SizedBox(height:10),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.05), // Opacidade bem baixa para sombra suave
+                      offset: Offset(3, 3), // Deslocamento suave
+                      blurRadius: 12, // Desfoque mais suave
+                      spreadRadius: 0, // Nenhuma expansão da sombra
+                    ),
+                  ]
+              ),
+              height: 150,
+            ),
+            SizedBox(height: 10),
+            Container(
+              height:250 ,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.white
+              ),
+            )
           ],
+
         ),
       ),
     );
