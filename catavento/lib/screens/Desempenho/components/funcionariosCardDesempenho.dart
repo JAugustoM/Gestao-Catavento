b2d094ec21cba8754bae0ccabee117fbdc91b1d7
Leticia-Arisa-K-Higa <higaleticia11@gmail.com>
2025-01-04 11:49:47 -0300

    feat: dialog info desempenho

--- catavento/lib/screens/Desempenho/components/funcionariosCardDesempenho.dart
+++ catavento/lib/screens/Desempenho/components/funcionariosCardDesempenho.dart
@@ -1,4 +1,5 @@
 import 'package:catavento/shared/theme/colors.dart';
+import 'package:catavento/shared/widgets/dialog.dart';
 import 'package:flutter/material.dart';
 
 class FuncionariosCardDesempenho extends StatefulWidget {
@@ -17,6 +18,11 @@ class FuncionariosCardDesempenho extends StatefulWidget {
 }
 
 class FuncionariosCardDesempenhoState extends State<FuncionariosCardDesempenho> {
+  //Dados temporarios
+  final String nome = '{nomeFuncionario}';
+  final String setor = '{setor}';
+  final String mediaDesempenho = '00:00';
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -59,11 +65,148 @@ class FuncionariosCardDesempenhoState extends State<FuncionariosCardDesempenho> 
           IconButton(
             icon: Icon(Icons.info, color: AppColors.blue,),
             onPressed: () {
-
+              _showInfoDialog(context, nome, setor, mediaDesempenho);
             },
           )
         ],
       ),
+    );
+  }
+
+  void _showInfoDialog(BuildContext context, String nome, String setor,
+      String mediaDesempenho) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return ReusableDialog(
+          backgroundColor: AppColors.lightGray,
+          title: nome,
+          body: SingleChildScrollView(
+            // Adicionando SingleChildScrollView para evitar overflow
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.circular(16), // Bordas arredondadas
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Código
+                      Row(
+                        children: [
+                          Icon(Icons.person, color: AppColors.blue,),
+                          Text(
+                            'Dados',
+                            style: TextStyle(
+                              color: AppColors.blue,
+                              fontSize: 16
+                            ),
+                          )
+                        ],
+                      ),
+                      Center(
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.17,
+                          width: MediaQuery.of(context).size.width * 0.1,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(70),
+                            color: AppColors.lightGray
+                          ),
+                          child: Icon(Icons.person, color: AppColors.blue, size: 70,),
+                        )
+                      ),
+                      SizedBox(height: MediaQuery.of(context).size.height * 0.02,),
+                      Center(
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  'Nome: ',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    color: AppColors.blue,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                                Text(
+                                  nome,
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    color: AppColors.blue
+                                  ),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: MediaQuery.of(context).size.height * 0.01,),
+                            Row(
+                              children: [
+                                Text(
+                                  'Setor: ',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    color: AppColors.blue,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                                Text(
+                                  setor,
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    color: AppColors.blue
+                                  ),
+                                )
+                              ],
+                            ),
+                            SizedBox(height: MediaQuery.of(context).size.height * 0.01,),
+                            Row(
+                              children: [
+                                InkWell(
+                                  onTap: () {
+                                    
+                                  },
+                                  child: Text(
+                                  'Desempenho: ',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    color: AppColors.blue,
+                                    fontWeight: FontWeight.bold,
+                                    decoration: TextDecoration.underline,
+                                  ),
+                                ),
+                                ),
+
+                                InkWell(
+                                  onTap: () {
+                                    
+                                  },
+                                  child: Text(
+                                  mediaDesempenho,
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    color: AppColors.blue,
+                                    decoration: TextDecoration.underline
+                                  ),
+                                ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
