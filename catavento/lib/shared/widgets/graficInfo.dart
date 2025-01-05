029245170e865d448ac1fc34556c2ed0266a9946
Danielle Soares <silvasoaresdanielle2@gmail.com>
2025-01-05 17:51:55 -0300

    update: adaptação de dialog de adicionar funcionario a  reusable dialog e cards de estatística de funcionários descomentado

--- catavento/lib/shared/widgets/graficInfo.dart
+++ catavento/lib/shared/widgets/graficInfo.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:catavento/shared/theme/colors.dart';
 
 class Graficinfo<B extends BlocBase<S>, S> extends StatefulWidget {
   final double? size;
@@ -10,14 +10,13 @@ class Graficinfo<B extends BlocBase<S>, S> extends StatefulWidget {
   final String dataKey;
   final BlocBuilderCondition<S> buildWhen;
 
-  Graficinfo ({
+  Graficinfo({
     this.size,
     required this.icons,
     required this.colorIcons,
     required this.info,
     required this.dataKey,
     required this.buildWhen,
-
   });
 
   @override
@@ -26,48 +25,56 @@ class Graficinfo<B extends BlocBase<S>, S> extends StatefulWidget {
   }
 }
 
-class GraficinfoState<B extends BlocBase<S>, S> extends State<Graficinfo<B, S>> {
+class GraficinfoState<B extends BlocBase<S>, S>
+    extends State<Graficinfo<B, S>> {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<B, S>(
-      buildWhen: widget.buildWhen,
-      builder: (context, response) {
-        final metaData = (response as dynamic).metaData;
+        buildWhen: widget.buildWhen,
+        builder: (context, response) {
+          final metaData = (response as dynamic).metaData;
 
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(
-              widget.icons,
-              size: widget.size,
-              color: widget.colorIcons,
-            ),
-            SizedBox(width: 20),
-
-            Center(  // Adicionado Center para centralizar o conteúdo dentro do Column
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,  // Centralizado horizontalmente
-                children: [
-                  Text(
-                    "${metaData[widget.dataKey] ?? '--'}",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
+          return Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  widget.icons,
+                  size: widget.size,
+                  color: widget.colorIcons,
+                ),
+                SizedBox(width: 20),
+                Center(
+                  // Adicionado Center para centralizar o conteúdo dentro do Column
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment
+                        .start, // Centralizado horizontalmente
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "${metaData[widget.dataKey] ?? '--'}",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontFamily: 'FredokaOne',
+                          color: AppColors.gradientDarkBlue,
+                        ),
+                      ),
+                      SizedBox(height: 2),
+                      Text(
+                        widget.info,
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.gradientDarkBlue),
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 2),
-                  Text(
-                    widget.info,
-                    style: TextStyle(fontSize: 18, color: Colors.black),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ]
-        );
-      }
-    );
+          );
+        });
   }
 }
