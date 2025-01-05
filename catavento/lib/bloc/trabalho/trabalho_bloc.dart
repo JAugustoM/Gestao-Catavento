41e1eb02ec2f8d7890dbb1ff54c8277ab38cc6fd
JAugustoM <joseaugustomoraes@outlook.com>
2025-01-05 18:42:32 -0300

    [FEAT] Bloc trabalho inicializado

--- catavento/lib/bloc/trabalho/trabalho_bloc.dart
+++ catavento/lib/bloc/trabalho/trabalho_bloc.dart
@@ -1,2 +1,27 @@
+import 'package:catavento/typedefs.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 part 'trabalho_event.dart';
 part 'trabalho_state.dart';
+
+class TrabalhoBloc extends Bloc<TrabalhoEvent, TrabalhoState> {
+  final _supabase = Supabase.instance.client;
+  DatabaseResponse _currentData = [];
+
+  TrabalhoEvent get initialState => TrabalhoLoading();
+
+  TrabalhoBloc() : super(TrabalhoLoadingState([], {})) {
+    on<TrabalhoLoading>(_onLoading);
+
+    on<TrabalhoInit>(_onInit);
+
+    on<TrabalhoFinish>(_onFinish);
+  }
+
+  void _onLoading(TrabalhoLoading event, Emitter<TrabalhoState> emit) {}
+
+  void _onInit(TrabalhoInit event, Emitter<TrabalhoState> emit) {}
+
+  void _onFinish(TrabalhoFinish event, Emitter<TrabalhoState> emit) {}
+}
--- catavento/lib/bloc/trabalho/trabalho_state.dart
+++ catavento/lib/bloc/trabalho/trabalho_state.dart
@@ -1,5 +1,20 @@
 part of 'trabalho_bloc.dart';
 
 sealed class TrabalhoState {
-  const TrabalhoState();
+  final DatabaseResponse databaseResponse;
+  final Map<String, int> metaData;
+
+  const TrabalhoState(this.databaseResponse, this.metaData);
+}
+
+class TrabalhoInitState extends TrabalhoState {
+  TrabalhoInitState(super.databaseResponse, super.metaData);
+}
+
+class TrabalhoFinishState extends TrabalhoState {
+  TrabalhoFinishState(super.databaseResponse, super.metaData);
+}
+
+class TrabalhoLoadingState extends TrabalhoState {
+  TrabalhoLoadingState(super.databaseResponse, super.metaData);
 }
