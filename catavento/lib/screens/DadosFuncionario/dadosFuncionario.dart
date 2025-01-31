c95d2a81e9374b2dbf0c81e283788a8f8979a816
Danielle Soares <silvasoaresdanielle2@gmail.com>
2025-01-31 16:34:49 -0300

    fix: menu da página de desempenho, do perfil de funcionários, corrigido

--- catavento/lib/screens/DadosFuncionario/dadosFuncionario.dart
+++ catavento/lib/screens/DadosFuncionario/dadosFuncionario.dart
@@ -13,58 +13,56 @@ class Dadosfuncionario extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Navbar(),
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: Builder(
-          builder: (context) => IconButton(
-            onPressed: () => {
-              Scaffold.of(context).openDrawer(),
-            },
-            icon: Icon(Icons.tune, color: Colors.black),
-            highlightColor: Colors.transparent,
-            splashColor: Colors.transparent,
-            hoverColor: Colors.transparent,
+      body: Row(
+        children: [
+          Container(
+            width: 300,
+            color: Colors.transparent,
+            child: Navbar(),
           ),
-        ),
-      ),
-      body: Container(
-        color: Colors.white,
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocBuilder<AuthBloc, AuthState>(
-              builder: (context, state) {
-                Map<String, dynamic> userData = {};
-                if (state is AuthAuthenticated) {
-                  userData = context.read<AuthBloc>().userData;
-                }
-                return WidgetDadosFuncionario(
-                  nome: userData['nome'] ?? "Nome",
-                  nickname: userData['usuario'] ?? "Usuario",
-                  email: userData['email'] ?? "Email",
-                  setor: userData['setor'] ?? "Setor",
-                );
-              },
+          Expanded(
+            child: Center(
+              child: Container(
+                color: Colors.white,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    BlocBuilder<AuthBloc, AuthState>(
+                      builder: (context, state) {
+                        Map<String, dynamic> userData = {};
+                        if (state is AuthAuthenticated) {
+                          userData = context.read<AuthBloc>().userData;
+                        }
+                        return WidgetDadosFuncionario(
+                          nome: userData['nome'] ?? "Nome",
+                          nickname: userData['usuario'] ?? "Usuario",
+                          email: userData['email'] ?? "Email",
+                          setor: userData['setor'] ?? "Setor",
+                        );
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    BlocBuilder<TrabalhoBloc, TrabalhoState>(
+                      builder: (context, state) {
+                        final metaData = state.metaData;
+                        return Widgetdesempenho(
+                          data: DateFormat('dd/MM/yyyy').format(DateTime.now()),
+                          goal: metaData['total'] ?? 0,
+                          isCompleted: metaData['completo'] ?? 0,
+                          isMissing: metaData['faltam'] ?? 0,
+                        );
+                      },
+                    )
+                  ],
+                ),
+              ),
             ),
-            SizedBox(
-              height: 20,
-            ),
-            BlocBuilder<TrabalhoBloc, TrabalhoState>(
-              builder: (context, state) {
-                final metaData = state.metaData;
-                return Widgetdesempenho(
-                  data: DateFormat('dd/MM/yyyy').format(DateTime.now()),
-                  goal: metaData['total'] ?? 0,
-                  isCompleted: metaData['completo'] ?? 0,
-                  isMissing: metaData['faltam'] ?? 0,
-                );
-              },
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
