e4615db54651771a2027f9c209aa9e05949d7160
dudaa28 <eduardaq2805@gmail.com>
2025-02-01 16:31:46 -0300

    adicionando arquivos mock para testes unitarios

---
+++ catavento/test/mocks/mock_usuario_bloc/mock_usuario_bloc.dart
@@ -0,0 +1,240 @@
+// import 'dart:io';
+
+// import 'package:catavento/bloc/demanda_bloc.dart';
+// import 'package:catavento/constants.dart';
+// import 'package:catavento/bloc/demanda_bloc.dart';
+import 'package:catavento/typedefs.dart';
+import 'package:equatable/equatable.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:http/http.dart';
+// import 'package:intl/intl.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+part 'mock_usuario_event.dart';
+part 'mock_usuario_state.dart';
+
+class UsuarioBloc extends Bloc<UsuarioEvent, UsuarioState> {
+  final SupabaseClient supabase;
+  DatabaseResponse _currentData = [];
+  UsuarioEvent get initialState => UsuarioLoading();
+
+  UsuarioBloc(this.supabase) : super(UsuarioLoadingState([], {})) {
+    on<UsuarioLoading>(_onLoading);
+
+    on<UsuarioCreate>(_onCreate);
+
+    on<UsuarioDelete>(_onDelete);
+
+    on<UsuarioUpdate>(_onUpdate);
+  }
+
+  void _onCreate(UsuarioCreate event, Emitter<UsuarioState> emit) async {
+    final usuario = {
+      'nome': event.nome,
+      'usuario': event.usuario,
+      'email': event.email,
+      'setor': event.setor,
+      'tipo': event.tipo
+    };
+
+    if (usuario['email'] != null && usuario['email']!.isNotEmpty) {
+      try {
+        final user = supabase.auth.currentUser;
+        final userData = await supabase
+            .from('usuarios')
+            .select('tipo')
+            .eq('email', user!.email!);
+        final userTipo = userData.first['tipo'] as String;
+
+        if (userTipo != 'admin' && event.tipo == 'gerente') {
+          final metaData = _countUsuarios();
+          emit(UsuarioErrorState(_currentData, metaData,
+              'Apenas o admin pode cadastrar gerentes'));
+        }
+
+        final authResponse = await supabase.auth.signUp(
+          email: event.email,
+          password: event.senha,
+        );
+        if (authResponse.user != null) {
+          final response =
+              await supabase.from('usuarios').insert(usuario).select();
+          if (response.isNotEmpty) {
+            _currentData.add(response[0]);
+          } else {
+            final metaData = _countUsuarios();
+            emit(UsuarioErrorState(
+              _currentData,
+              metaData,
+              "Erro ao criar usuário",
+            ));
+          }
+        }
+      } catch (e) {
+        final metaData = _countUsuarios();
+        emit(UsuarioErrorState(
+          _currentData,
+          metaData,
+          "Erro ao criar novo usuário - $e",
+        ));
+      }
+    }
+
+    final metaData = _countUsuarios();
+
+    emit(UsuarioCreateState(_currentData, metaData));
+  }
+
+  void _onDelete(UsuarioDelete event, Emitter<UsuarioState> emit) async {
+    try {
+      final response = await supabase
+          .from('usuarios')
+          .delete()
+          .eq('email', event.email)
+          .select();
+      if (response.isNotEmpty) {
+        _currentData.removeAt(event.order);
+      }
+    } catch (e) {
+      final metaData = _countUsuarios();
+      emit(UsuarioErrorState(
+        _currentData,
+        metaData,
+        "Erro ao remover usuário - $e",
+      ));
+    }
+
+    final metaData = _countUsuarios();
+
+    emit(UsuarioDeleteState(_currentData, metaData));
+  }
+
+  void _onUpdate(UsuarioUpdate event, Emitter<UsuarioState> emit) async {
+    try {
+      final nome = event.nome;
+      final usuario = event.usuario;
+      final tipo = event.tipo;
+      final setor = event.setor;
+      final email = event.email;
+      final id = event.id;
+
+      try {
+        final user = supabase.auth.currentUser;
+        final userData = await supabase
+            .from('usuarios')
+            .select('tipo')
+            .eq('email', user!.email!);
+        final userTipo = userData.first['tipo'] as String;
+
+        if (userTipo != 'admin' && tipo == 'gerente') {
+          final metaData = _countUsuarios();
+          emit(UsuarioErrorState(_currentData, metaData,
+              'Apenas o admin pode cadastrar gerentes'));
+        }
+      } catch (e) {
+        final metaData = _countUsuarios();
+        emit(UsuarioErrorState(
+            _currentData, metaData, 'Erro ao acessar o banco de dados - $e'));
+      }
+
+      Map<String, dynamic> atualizado = {};
+
+      if (nome.isNotEmpty) {
+        atualizado['nome'] = nome;
+      }
+
+      if (usuario.isNotEmpty) {
+        atualizado['usuario'] = usuario;
+      }
+
+      if (tipo.isNotEmpty) {
+        atualizado['tipo'] = tipo;
+      }
+
+      if (setor.isNotEmpty) {
+        atualizado['setor'] = setor;
+      }
+
+      if (email.isNotEmpty) {
+        atualizado['email'] = email;
+      }
+
+      if (atualizado.isNotEmpty) {
+        await supabase
+            .from('usuarios')
+            .update(atualizado)
+            .eq('email', event.email);
+      }
+
+      // verificar se pode usar o id
+
+      for (var key in atualizado.keys) {
+        _currentData[id][key] = atualizado[key];
+      }
+
+      final metaData = _countUsuarios();
+
+      emit(UsuarioUpdateState(_currentData, metaData));
+    } catch (e) {
+      final metaData = _countUsuarios();
+      emit(UsuarioErrorState(
+        _currentData,
+        metaData,
+        "Erro ao atualizar usuário - $e",
+      ));
+    }
+  }
+
+  void _onLoading(UsuarioLoading event, Emitter<UsuarioState> emit) async {
+    try {
+      final response =
+          await supabase.from('usuarios').select().neq('tipo', 'admin');
+      _currentData = response;
+
+      final metaData = _countUsuarios();
+
+      emit(UsuarioLoadingState(_currentData, metaData));
+    } catch (e) {
+      final metaData = _countUsuarios();
+      emit(UsuarioErrorState(
+        _currentData,
+        metaData,
+        "Erro ao acessar o banco de dados - $e",
+      ));
+    }
+  }
+
+  Map<String, int> _countUsuarios() {
+    int numUsuarios = 0;
+
+    for (var usuario in _currentData) {
+      if (usuario["tipo"] != "admin") {
+        numUsuarios++;
+      }
+    }
+    final metaData = {"total": numUsuarios};
+    return metaData;
+  }
+
+  Map<String, dynamic>? getUser(String email) {
+    Map<String, dynamic>? user;
+    int id = 0;
+
+    for (var data in _currentData) {
+      if (data['email'] == email) {
+        user = data;
+        break;
+      }
+      id++;
+    }
+
+    if (user != null) {
+      user['id'] = id;
+    }
+
+    return user;
+  }
+}
---
+++ catavento/test/mocks/mock_usuario_bloc/mock_usuario_event.dart
@@ -0,0 +1,57 @@
+part of 'mock_usuario_bloc.dart';
+
+sealed class UsuarioEvent {
+  const UsuarioEvent();
+}
+
+class UsuarioCreate extends UsuarioEvent {
+  final String nome;
+  final String usuario;
+  final String setor;
+  final String email;
+  final String tipo;
+  final String senha;
+
+  const UsuarioCreate(
+    this.nome,
+    this.usuario,
+    this.setor,
+    this.email,
+    this.tipo,
+    this.senha,
+  );
+}
+
+class UsuarioDelete extends UsuarioEvent {
+  final int order;
+  final String email;
+
+  const UsuarioDelete(
+    this.order,
+    this.email,
+  );
+}
+
+class UsuarioUpdate extends UsuarioEvent {
+  final int id;
+  final String nome;
+  final String setor;
+  final String tipo;
+  final String email;
+  final String usuario;
+  final String senha;
+
+  const UsuarioUpdate(
+    this.nome,
+    this.setor,
+    this.tipo,
+    this.email,
+    this.usuario,
+    this.id,
+    this.senha,
+  );
+}
+
+class UsuarioLoading extends UsuarioEvent {}
+
+class UsuerioError extends UsuarioEvent {}
